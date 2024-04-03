2e183819349247b8250d1490b7d3b3ec3a676af5
Alen Šimunic <alen.simunic@appliment.eu>
2024-04-03 09:09:57 +0200

    npm run build fix

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,19 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
-        plugins: [vue(), laravel(["public/layout/styles/preloading/preloading.css", "public/layout/styles/theme/theme-light/blue/theme.css", "src/main.js"])],
+        plugins: [
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+            laravel({
+                input: ["public/layout/styles/preloading/preloading.css", "public/layout/styles/theme/theme-light/blue/theme.css", "public/layout/styles/theme/theme-light/teal/theme.css", "src/main.js"],
+                refresh: true
+            })
+        ],
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url)),
