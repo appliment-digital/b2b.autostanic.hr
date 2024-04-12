2d042febcdf830954f6d1cda4350ccfbf170a336
hmlinarevic-appliment <hrvoje.mlinarevic@appliment.eu>
2024-04-12 12:15:35 +0200

    #21216: add reset password page

---
+++ src/views/pages/ResetPassword.vue
@@ -0,0 +1,64 @@
+<script>
+import router from '@/router';
+
+export default {
+    data() {
+        return {};
+    },
+    methods: {
+        changeMessage() {
+            this.message = 'Vue.js is awesome!';
+        },
+
+        handleForgotPasswordClick() {
+            // router.push('/forgot');
+            console.log('hi');
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="flex flex-column sm:w-3 mx-auto">
+        <h2>Postavi Lozinku</h2>
+
+        <div class="mt-6">
+            <InputText
+                class="block w-full mb-3"
+                type="text"
+                v-model="value"
+                placeholder="email"
+            />
+
+            <InputText
+                class="block w-full mb-3"
+                type="text"
+                v-model="value"
+                placeholder="verification code"
+            />
+
+            <InputText
+                class="block w-full mb-3"
+                type="text"
+                v-model="value"
+                placeholder="new password"
+            />
+
+            <InputText
+                class="block w-full mb-5"
+                type="text"
+                v-model="value"
+                placeholder="confirm password"
+            />
+
+            <Button
+                class="block w-full"
+                label="Reset"
+                @click="handleResetButtonClick"
+            />
+        </div>
+
+    </div>
+</template>
+
+<style scoped></style>
