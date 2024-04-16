ed4526b8f3ba4a7f300003f571efc8c45b227b68
LarisaTunjicAppliment <larisa.tunjic@appliment.eu>
2024-04-16 15:57:59 +0200

    #21602  Forgot i reset password

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,8 @@ use App\Http\Controllers\DiscountTypeController;
 
 //****************  AUTH CONTOLLER  ******************
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'forgotPassword']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
