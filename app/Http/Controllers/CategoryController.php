a30f805d32c0b6fda8db273d36437dd1fae8c58f
LarisaTunjicAppliment <larisa.tunjic@appliment.eu>
2024-04-25 09:02:12 +0200

    #22020 Izmjeniti relaciju između tipova rabata i korisnika na 1-> više

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,14 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Category;
 use Exception;
+use Illuminate\Support\Facades\DB;
 class CategoryController extends BaseController
 {
     public function getMainCategories()
     {
         try {
-            return Category::getMainCategories();
+            $query = DB::connection('webDb')
+                ->table('dbo.Category')
+                ->select(
+                    'Id',
+                    'Name',
+                    'Description',
+                    'ParentCategoryId',
+                    'ProductCount'
+                )
+                ->where('ParentCategoryId', 0)
+                ->get();
+
+            return $query;
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Exception: ' . $e->getMessage(),
+            ]);
+        }
+    }
+
+    public function getSubcategories($id)
+    {
+        try {
+            $query = DB::connection('webDb')
+                ->table('dbo.Category')
+                ->select(
+                    'Id',
+                    'Name',
+                    'Description',
+                    'ParentCategoryId',
+                    'ProductCount'
+                )
+                ->where('ParentCategoryId', $id)
+                ->get();
+
+            return $query;
         } catch (Exception $e) {
             return response()->json([
                 'error' => 'Exception: ' . $e->getMessage(),
