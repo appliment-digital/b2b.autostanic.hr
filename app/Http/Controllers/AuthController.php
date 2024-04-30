f214dd0b70ffa643d623cb9deffd4fdb6fbd9cdb
LarisaTunjicAppliment <larisa.tunjic@appliment.eu>
2024-04-30 10:16:38 +0200

    In login function add role

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,36 +11,52 @@ use Laravel\Sanctum\HasApiTokens;
 use App\Models\Role;
 use Illuminate\Support\Facades\Mail;
 
-
 class AuthController extends BaseController
 {
     public function login(Request $request)
     {
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-
+        if (
+            Auth::attempt([
+                'email' => $request->email,
+                'password' => $request->password,
+            ])
+        ) {
             $authUser = Auth::user();
 
             if ($authUser->active == false) {
-                return response()->json(['error' => 'Nemate pravo pristupa.'], 401);
+                return response()->json(
+                    ['error' => 'Nemate pravo pristupa.'],
+                    401
+                );
             }
 
-            $success['token'] =  $authUser->createToken('MyAuthApp')->plainTextToken;
-            $success['id'] =  $authUser->id;
-            $success['name'] =  $authUser->name;
-            $success['last_name'] =  $authUser->last_name;
-            $success['email'] =  $authUser->email;
+            $authUserWithRoles = $authUser->load('roles');
+
+            $success['token'] = $authUser->createToken(
+                'MyAuthApp'
+            )->plainTextToken;
+            $success['id'] = $authUser->id;
+            $success['name'] = $authUser->name;
+            $success['last_name'] = $authUser->last_name;
+            $success['email'] = $authUser->email;
+            $success['role'] = $authUserWithRoles->roles;
 
             return $this->sendResponse($success, 'Dobro došli');
         } else {
-            return response()->json(['error' => 'Email ili lozinka koju ste unijeli nisu ispravni.'], 401);
+            return response()->json(
+                [
+                    'error' =>
+                        'Email ili lozinka koju ste unijeli nisu ispravni.',
+                ],
+                401
+            );
         }
     }
-
 
     public function logout(Request $request)
     {
         $request->session()->flush();
-        $id = (int)explode('|', $request->token)[0];
+        $id = (int) explode('|', $request->token)[0];
         if (auth()->user()->tokens()->where('id', $id)->delete()) {
             return ['message' => 'Logged out'];
         }
@@ -51,17 +67,24 @@ class AuthController extends BaseController
         try {
             return auth()->user()->name;
         } catch (Exception $e) {
-            return response()->json(['error' => 'Exception: ' . $e->getMessage()]);
+            return response()->json([
+                'error' => 'Exception: ' . $e->getMessage(),
+            ]);
         }
     }
 
     public function getRoles(Request $request)
     {
         try {
-
-            return response()->json(['data' => auth()->user()->roles->map->only(['name'])]);
+            return response()->json([
+                'data' => auth()
+                    ->user()
+                    ->roles->map->only(['name']),
+            ]);
         } catch (Exception $e) {
-            return response()->json(['error' => 'Exception: ' . $e->getMessage()]);
+            return response()->json([
+                'error' => 'Exception: ' . $e->getMessage(),
+            ]);
         }
     }
 
@@ -69,55 +92,75 @@ class AuthController extends BaseController
     {
         try {
             $request->validate([
-                'email' => 'required|email'
+                'email' => 'required|email',
             ]);
-
 
             $user = User::where('email', $request->email)->first();
             if (!$user) {
-
-                return response()->json([
-                    'message' => 'Wrong email adress',
-                    'status_code' => 200
-                ], 200);
+                return response()->json(
+                    [
+                        'message' => 'Wrong email adress',
+                        'status_code' => 200,
+                    ],
+                    200
+                );
             } else {
                 $random = rand(111111, 999999);
 
                 $user->verification_code = $random;
                 if ($user->save()) {
-                    $userData = array(
+                    $userData = [
                         'email' => $user->email,
-                        'full_name' => $user->name . " " . $user->last_name,
-                        'random' => $random
-                    );
+                        'full_name' => $user->name . ' ' . $user->last_name,
+                        'random' => $random,
+                    ];
                     // return $userData;
-                    Mail::send('emails.reset_password', $userData, function ($message) use ($userData) {
-                        $message->from('sales@autostanic.hr', 'Resetiranje lozinke');
+                    Mail::send('emails.reset_password', $userData, function (
+                        $message
+                    ) use ($userData) {
+                        $message->from(
+                            'sales@autostanic.hr',
+                            'Resetiranje lozinke'
+                        );
 
-                        $message->to($userData['email'], $userData['full_name']);
+                        $message->to(
+                            $userData['email'],
+                            $userData['full_name']
+                        );
                         $message->subject('Zahtjev za resetiranje lozinke');
                     });
 
-                    return response()->json([
-                        'message' => 'We have sent a verification code to your email address',
-                        'status_code' => 200
-                    ], 200);
+                    return response()->json(
+                        [
+                            'message' =>
+                                'We have sent a verification code to your email address',
+                            'status_code' => 200,
+                        ],
+                        200
+                    );
                 } else {
-                    return response()->json([
-                        'message' => 'Some error occurred, Please try again',
-                        'status_code' => 200
-                    ], 200);
+                    return response()->json(
+                        [
+                            'message' =>
+                                'Some error occurred, Please try again',
+                            'status_code' => 200,
+                        ],
+                        200
+                    );
                 }
             }
         } catch (\Exception $e) {
-            return response()->json([
-                'message' => 'Some error occurred while sending the email: ' . $e->getMessage(),
-                'status_code' => 500
-            ], 500);
+            return response()->json(
+                [
+                    'message' =>
+                        'Some error occurred while sending the email: ' .
+                        $e->getMessage(),
+                    'status_code' => 500,
+                ],
+                500
+            );
         }
     }
-
-
 
     public function resetPassword(Request $request)
     {
@@ -127,28 +170,39 @@ class AuthController extends BaseController
             'password' => 'required|confirmed|min:6',
         ]);
 
-        $user = User::where('email', $request->email)->where('verification_code', $request->verification_code)->first();
+        $user = User::where('email', $request->email)
+            ->where('verification_code', $request->verification_code)
+            ->first();
         if (!$user) {
-            return response()->json([
-                'error' => 'error',
-                'message' => 'User not found/Invalid code',
-                'status_code' => 401
-            ], 401);
+            return response()->json(
+                [
+                    'error' => 'error',
+                    'message' => 'User not found/Invalid code',
+                    'status_code' => 401,
+                ],
+                401
+            );
         } else {
             $user->password = bcrypt(trim($request->password));
-            $user->verification_code = Null;
+            $user->verification_code = null;
 
             if ($user->save()) {
-                return response()->json([
-                    'message' => 'Password updated successfully!',
-                    'status_code' => 200
-                ], 200);
+                return response()->json(
+                    [
+                        'message' => 'Password updated successfully!',
+                        'status_code' => 200,
+                    ],
+                    200
+                );
             } else {
-                return response()->json([
-                    'error' => 'error',
-                    'message' => 'Some error occurred, Please try again',
-                    'status_code' => 500
-                ], 500);
+                return response()->json(
+                    [
+                        'error' => 'error',
+                        'message' => 'Some error occurred, Please try again',
+                        'status_code' => 500,
+                    ],
+                    500
+                );
             }
         }
     }
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -20,7 +20,7 @@ class CategoryController extends BaseController
                     'Category.PictureId',
                     'Category.Breadcrumb',
                     'Category.DisplayOrder',
-                    'Picture.SeoFilename' // Add the SeoFilename column from the Picture table
+                    'Picture.SeoFilename'
                 )
                 ->leftJoin(
                     'dbo.Category_Picture_Mapping',
@@ -39,20 +39,16 @@ class CategoryController extends BaseController
                 ->where('Category.Deleted', 0)
                 ->where('Category.Published', 1)
                 ->orderBy('Category.DisplayOrder')
-                ->orderBy('Category.Id') // Add additional order by statement
+                ->orderBy('Category.Id')
                 ->distinct()
                 ->get()
-                ->toArray(); // Convert the collection of objects to array
+                ->toArray();
 
-            // Initialize an empty array to store the final response
             $response = [];
 
-            // Group categories by ID
             $categoriesById = collect($query)->groupBy('Id');
 
-            // Iterate over categories
             foreach ($categoriesById as $categoryId => $categories) {
-                // Initialize an array to store category data
                 $categoryData = [
                     'Id' => $categoryId,
                     'Name' => $categories[0]->Name,
@@ -64,10 +60,8 @@ class CategoryController extends BaseController
                     'SeoFilename' => $categories[0]->SeoFilename,
                 ];
 
-                // Initialize an array to store picture URLs for this category
                 $pictureUrls = [];
 
-                // Construct URLs for each picture of the category
                 foreach ($categories as $category) {
                     $paddedPictureId = str_pad(
                         $category->PictureId,
@@ -78,10 +72,8 @@ class CategoryController extends BaseController
                     $pictureUrls[] = "https://www.autostanic.hr/content/images/thumbs/{$paddedPictureId}_{$category->SeoFilename}_170.jpg";
                 }
 
-                // Add picture URLs array to category data
                 $categoryData['picture_urls'] = $pictureUrls;
 
-                // Add category data to the final response
                 $response[] = $categoryData;
             }
 
@@ -127,81 +119,5 @@ class CategoryController extends BaseController
 
     public function test()
     {
-        $query = DB::connection('webshopdb')
-            ->table('dbo.Category')
-            ->select(
-                'Category.Id',
-                'Category.Name',
-                'Category.ParentCategoryId',
-                'Category.ProductCount',
-                'Category.PictureId',
-                'Category.Breadcrumb',
-                'Category.DisplayOrder',
-                'Picture.SeoFilename' // Add the SeoFilename column from the Picture table
-            )
-            ->leftJoin(
-                'dbo.Category_Picture_Mapping',
-                'Category_Picture_Mapping.CategoryId',
-                '=',
-                'Category.Id'
-            )
-            ->leftJoin(
-                'dbo.Picture',
-                'Category_Picture_Mapping.PictureId',
-                '=',
-                'Picture.Id'
-            )
-            ->where('Category.ParentCategoryId', 0)
-            ->where('Category.ShowOnHomePage', 1)
-            ->where('Category.Deleted', 0)
-            ->where('Category.Published', 1)
-            ->orderBy('Category.DisplayOrder')
-            ->orderBy('Category.Id') // Add additional order by statement
-            ->distinct()
-            ->get()
-            ->toArray(); // Convert the collection of objects to array
-
-        // Initialize an empty array to store the final response
-        $response = [];
-
-        // Group categories by ID
-        $categoriesById = collect($query)->groupBy('Id');
-
-        // Iterate over categories
-        foreach ($categoriesById as $categoryId => $categories) {
-            // Initialize an array to store category data
-            $categoryData = [
-                'Id' => $categoryId,
-                'Name' => $categories[0]->Name,
-                'ParentCategoryId' => $categories[0]->ParentCategoryId,
-                'ProductCount' => $categories[0]->ProductCount,
-                'PictureId' => $categories[0]->PictureId,
-                'Breadcrumb' => $categories[0]->Breadcrumb,
-                'DisplayOrder' => $categories[0]->DisplayOrder,
-                'SeoFilename' => $categories[0]->SeoFilename,
-            ];
-
-            // Initialize an array to store picture URLs for this category
-            $pictureUrls = [];
-
-            // Construct URLs for each picture of the category
-            foreach ($categories as $category) {
-                $paddedPictureId = str_pad(
-                    $category->PictureId,
-                    7,
-                    '0',
-                    STR_PAD_LEFT
-                );
-                $pictureUrls[] = "https://www.autostanic.hr/content/images/thumbs/{$paddedPictureId}_{$category->SeoFilename}_170.jpg";
-            }
-
-            // Add picture URLs array to category data
-            $categoryData['picture_urls'] = $pictureUrls;
-
-            // Add category data to the final response
-            $response[] = $categoryData;
-        }
-
-        return $response;
     }
 }
