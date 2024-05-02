e1ec3fd4956b4cd1c4eaaf3e8a2095c7729cff6e
LarisaTunjicAppliment <larisa.tunjic@appliment.eu>
2024-05-02 14:21:03 +0200

    chore: update vite js config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
+        esbuild: {
+            supported: {
+                'top-level-await': true, //browsers can handle top-level-await features
+            },
+        },
         plugins: [
             vue({
                 template: {
@@ -16,15 +21,20 @@ export default defineConfig(() => {
                 },
             }),
             laravel({
-                input: ["public/layout/styles/preloading/preloading.css", "public/layout/styles/theme/theme-light/blue/theme.css", "public/layout/styles/theme/theme-light/teal/theme.css", "src/main.js"],
-                refresh: true
-            })
+                input: [
+                    'public/layout/styles/preloading/preloading.css',
+                    'public/layout/styles/theme/theme-light/blue/theme.css',
+                    'public/layout/styles/theme/theme-light/teal/theme.css',
+                    'src/main.js',
+                ],
+                refresh: true,
+            }),
         ],
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url)),
-                '@public': fileURLToPath(new URL('./public', import.meta.url))
-            }
-        }
+                '@public': fileURLToPath(new URL('./public', import.meta.url)),
+            },
+        },
     };
 });
