b75010425051db93e4e4920a720fa40962d94bd9
hmlinarevic-appliment <hrvoje.mlinarevic@appliment.eu>
2024-05-03 14:07:19 +0200

    #22455: modify categories styles and car part search content

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -26,6 +26,13 @@ export default {
             .then((response) => {
                 if (response.data.length) {
                     this.categories = response.data;
+
+                    // update category name to be shorter for better UX
+                    this.categories.forEach((category) => {
+                        if (category.id == '39597') {
+                            category.name = 'Akustika i elektronika';
+                        }
+                    });
                 }
             })
             .catch((err) => console.error(err));
@@ -57,17 +64,21 @@ export default {
     </div>
 
     <!-- Home Page: Search & Categories -->
-    <div class="w-full mx-auto mt-3 grid gap-3">
+    <div class="w-full mx-auto mt-2 grid gap-2">
         <!-- Search -->
         <div
-            class="col-12 py-8 flex flex-column align-items-center justify-content-center surface-100 bg-white border-100 border-round border-1 md:col"
+            class="col-12 flex justify-content-center align-items-center surface-100 bg-white border-100 border-round border-1 md:col"
         >
             <div>
-                <!-- Checkboxes -->
-                <div
-                    class="flex flex-column column-gap-3 sm:col sm:flex-row md:flex-column"
+                <h3 class="text-700 mt-0 mb-2">Pretraži prema kodu</h3>
+                <span class="block text-500 mb-6"
+                    >Pretraži auto dijelove koristeći tvoj kod proizvoda za brzu
+                    pretragu.</span
                 >
-                    <div class="flex mb-3 md:mb-3">
+
+                <!-- Checkboxes -->
+                <div class="p-0 flex flex-column column-gap-3 sm:col sm:flex-row">
+                    <div class="flex mb-3 md:mb-3 align-items-center">
                         <Checkbox
                             v-model="pizza"
                             inputId="ingredient1"
@@ -78,7 +89,7 @@ export default {
                             >Broj artikla</label
                         >
                     </div>
-                    <div class="flex mb-3 md:mb-3">
+                    <div class="flex mb-3 md:mb-3 align-items-center">
                         <Checkbox
                             v-model="pizza"
                             inputId="ingredient1"
@@ -87,7 +98,7 @@ export default {
                         />
                         <label for="ingredient1" class="ml-2">Broj djela</label>
                     </div>
-                    <div class="flex mb-3 md:mb-3">
+                    <div class="flex mb-3 md:mb-3 align-items-center">
                         <Checkbox
                             v-model="pizza"
                             inputId="ingredient1"
@@ -98,7 +109,7 @@ export default {
                             >Šifra motora</label
                         >
                     </div>
-                    <div class="flex">
+                    <div class="flex align-items-center md:mb-3">
                         <Checkbox
                             v-model="pizza"
                             inputId="ingredient1"
@@ -113,17 +124,16 @@ export default {
 
                 <!-- Input -->
                 <div
-                    class="flex flex-column justify-content-between mt-5 gap-3 sm:flex-row md:flex-column lg:flex-row"
+                    class="flex flex-column justify-content-between mt-2 gap-2 sm:flex-row md:flex-column lg:flex-row"
                 >
                     <InputText
                         type="text"
-                        class="w-20rem sm:w-full md:w-20rem"
+                        class="w-20rem sm:w-full md:w-23rem"
                         placeholder="Upišite svoj kod ovdje"
                     />
                     <Button
                         label="pretraži"
                         class="button--submit block w-full sm:w-4 md:w-full lg:w-4"
-                        icon="pi pi-search"
                     />
                 </div>
             </div>
@@ -131,95 +141,103 @@ export default {
 
         <!-- Categories -->
         <div
-            class="col-12 flex flex-column py-8 bg-white border-100 border-round border-1 justify-content-center align-items-center md:col"
+            class="py-7 col-12 md:col bg-white border-100 border-round border-1 flex align-items-center"
         >
             <div
-                class="grid justify-content-center row-gap-4 column-gap-3 px-2"
+                class="grid justify-content-center row-gap-1 column-gap-1 px-2 align-items-start"
             >
                 <!-- prettier-ignore -->
                 <div
                     v-for="category in categories"
-                    class="col-4 flex flex-column justify-content-center            
-                    row-gap-2 p-4 shadow-2
-                    align-items-center cursor-pointer 
-                    border-round hover:shadow-5" 
+                    class="col-4 md:col-4 lg:col-3 flex flex-column justify-content-center            
+                    align-items-center cursor-pointer" 
                     @click="handleCategoryClick(category)"
                 >
-                    <img :src="category.pictureUrls[0]" style="width:82px" class="block mx-auto" >
-                    <span class="text-base text-center">{{ category.name }}</span>
+                    <div class="shadow-1 py-3 border-1 border-100 p-4 border-round hover:shadow-3">
+                        <img :src="category.pictureUrls[0]" style="width:62px" class="block mx-auto" >
+                    </div>
+                    <span class="text-sm text-center mt-2">{{ category.name }}</span>
                 </div>
             </div>
         </div>
     </div>
 
     <!-- Home Page: News -->
-    <div
-        class="mt-6 grid grid-nogutter row-gap-6 md:column-gap-0 lg:column-gap-3"
-    >
-        <div class="mx-auto col-12 sm:col-8 md:col">
-            <Card style="overflow: hidden" class="my-card cursor-pointer">
-                <template #header>
-                    <img
-                        src="https://source.unsplash.com/random/?Car&1"
-                        class="img--placeholder"
-                    />
-                </template>
-                <template #title>News 1</template>
-                <template #content>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Fuga earum harum non reiciendis ipsum! Voluptate
-                        quibusdam molestias consectetur odit dolorum earum, esse
-                        nemo inventore neque corporis ea voluptas dolorem culpa!
-                    </p>
-                </template>
-            </Card>
+    <section class="mt-2 bg-white border-1 border-100 border-round p-8">
+        <h3 class="text-700 mt-0 mb-2 text-center">Savjeti za Vas i Vaš automobil</h3>
+        <span class="block text-500 mb-7 text-center">Pročitajte kratke zanimljivosti vezane uz vašeg limenog ljubimca.</span>
+
+        <div
+            class="grid grid-nogutter row-gap-6 md:column-gap-0 lg:column-gap-3"
+        >
+            <div class="mx-auto col-12 sm:col-8 md:col">
+                <Card style="overflow: hidden" class="cursor-pointer border-1 border-100 shadow-2 hover:shadow-5">
+                    <template #header>
+                        <img
+                            src="https://source.unsplash.com/random/?Car&1"
+                            class="img--placeholder"
+                        />
+                    </template>
+                    <template #title>News 1</template>
+                    <template #content>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Fuga earum harum non reiciendis ipsum!
+                            Voluptate quibusdam molestias consectetur odit
+                            dolorum earum, esse nemo inventore neque corporis ea
+                            voluptas dolorem culpa!
+                        </p>
+                    </template>
+                </Card>
+            </div>
+            <div class="mx-auto col-12 sm:col-8 md:col">
+                <Card style="overflow: hidden" class="cursor-pointer border-1 border-100 shadow-2 hover:shadow-5">
+                    <template #header>
+                        <img
+                            src="https://source.unsplash.com/random/?Car&2"
+                            class="img--placeholder"
+                        />
+                    </template>
+                    <template #title>News 2</template>
+                    <template #content>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Fuga earum harum non reiciendis ipsum!
+                            Voluptate quibusdam molestias consectetur odit
+                            dolorum earum, esse nemo inventore neque corporis ea
+                            voluptas dolorem culpa!
+                        </p>
+                    </template>
+                    hello
+                </Card>
+            </div>
+            <div class="mx-auto col-12 sm:col-8 md:col">
+                <Card style="overflow: hidden" class="cursor-pointer border-1 border-100 shadow-2 hover:shadow-5">
+                    <template #header>
+                        <img
+                            src="https://source.unsplash.com/random/?Car&3"
+                            class="img--placeholder"
+                        />
+                    </template>
+                    <template #title>News 3</template>
+                    <template #content>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Fuga earum harum non reiciendis ipsum!
+                            Voluptate quibusdam molestias consectetur odit
+                            dolorum earum, esse nemo inventore neque corporis ea
+                            voluptas dolorem culpa!
+                        </p>
+                    </template>
+                </Card>
+            </div>
         </div>
-        <div class="mx-auto col-12 sm:col-8 md:col">
-            <Card style="overflow: hidden" class="my-card cursor-pointer">
-                <template #header>
-                    <img
-                        src="https://source.unsplash.com/random/?Car&2"
-                        class="img--placeholder"
-                    />
-                </template>
-                <template #title>News 2</template>
-                <template #content>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Fuga earum harum non reiciendis ipsum! Voluptate
-                        quibusdam molestias consectetur odit dolorum earum, esse
-                        nemo inventore neque corporis ea voluptas dolorem culpa!
-                    </p>
-                </template>
-                hello
-            </Card>
-        </div>
-        <div class="mx-auto col-12 sm:col-8 md:col">
-            <Card style="overflow: hidden" class="my-card cursor-pointer">
-                <template #header>
-                    <img
-                        src="https://source.unsplash.com/random/?Car&3"
-                        class="img--placeholder"
-                    />
-                </template>
-                <template #title>News 3</template>
-                <template #content>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Fuga earum harum non reiciendis ipsum! Voluptate
-                        quibusdam molestias consectetur odit dolorum earum, esse
-                        nemo inventore neque corporis ea voluptas dolorem culpa!
-                    </p>
-                </template>
-            </Card>
-        </div>
-    </div>
+    </section>
 
     <!-- Home Page: Footer -->
-    <footer>
+    <footer class="mt-3">
         <div
-            class="w-full mx-auto mt-6 p-8 bg-white border-round grid column-gap-2 row-gap-8 justify-content-center align-items-center border-100 border-round border-1 md:column-gap-4"
+            class="w-full mx-auto p-8 bg-white border-round grid column-gap-2 row-gap-8 justify-content-center align-items-center border-100 border-round border-1 md:column-gap-4"
         >
             <!-- Footer: Company Information -->
             <div class="col-12 md:col-5">
@@ -301,15 +319,6 @@ export default {
 .img--banner {
     width: 100%;
     object-fit: cover;
-}
-
-.my-card {
-    transition: box-shadow;
-    transition-duration: 150ms;
-}
-
-.my-card:hover {
-    box-shadow: 2px 2px 4px 2px rgba(0, 0, 0, 0.1);
 }
 
 .button--submit {
