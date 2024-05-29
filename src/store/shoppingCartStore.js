e8528cf42f75747864cd9ca3a2565799262dba85
hmlinarevic-appliment <hrvoje.mlinarevic@appliment.eu>
2024-05-29 08:15:24 +0200

    #23417: feat: update adding products to the cart and shopping cart style

--- src/store/shoppingCartStore.js
+++ src/store/shoppingCartStore.js
@@ -4,10 +4,6 @@ const calcTotal = (items) => {
     return items.reduce((total, item) => total + +item.price, 0);
 };
 
-// TODO: implement FeeService.js to get values
-const DELIVERY = 10000;
-const TAX = 10;
-
 export const useShoppingCartStore = defineStore('shoppingCart', {
     state: () => {
         return {
@@ -16,11 +12,6 @@ export const useShoppingCartStore = defineStore('shoppingCart', {
 
             currentTotalWithoutFees: 0,
             currentTotalWithFees: 0,
-
-            feeGroup: {
-                delivery: DELIVERY,
-                tax: TAX,
-            },
         };
     },
     getters: {
@@ -31,37 +22,22 @@ export const useShoppingCartStore = defineStore('shoppingCart', {
                 return JSON.parse(localStorage.getItem('shopping-cart'));
             }
         },
-        totalWithoutFees: (state) => {
-            const items = state.shoppingCart.length
-                ? state.shoppingCart
-                : JSON.parse(localStorage.getItem('shopping-cart'));
 
-            const total = items.reduce((total, item) => {
-                total += Number(item.price * item.quantity);
+        total: (state) => {
+            if (state.shoppingCart.length) {
+                const total = state.shoppingCart.reduce((total, product) => {
+
+                    total += Number(product.price * product.quantity);
+                    return total;
+                }, 0);
+
                 return total;
-            }, 0);
-
-            state.currentTotalWithoutFees = total;
-
-            return total.toFixed(2);
-        },
-
-        totalWithFees: (state) => {
-            if (state.currentTotalWithoutFees) {
-                return (state.currentTotalWithoutFees + DELIVERY + TAX).toFixed(
-                    2,
-                );
-            }
-        },
-
-        fees: (state) => {
-            if (state.feeGroup) {
-                return state.feeGroup;
             }
         },
     },
     actions: {
         add(product) {
+            // figure out if product already exists in the cart
             const entryId = this.shoppingCart.findIndex(
                 (entry) => entry.id == product.id,
             );
@@ -80,27 +56,10 @@ export const useShoppingCartStore = defineStore('shoppingCart', {
                 this.shoppingCart.push(product);
             }
 
-            console.log({ shoppingCart: this.shoppingCart });
-
             localStorage.setItem(
                 'shopping-cart',
                 JSON.stringify(this.shoppingCart),
             );
-        },
-
-        updateQuantity(product) {
-            const entryId = this.shoppingCart.findIndex(
-                (entry) => entry.id == product.id,
-            );
-
-            if (entryId != -1) {
-                this.shoppingCart[entryId].quantity = product.quantity;
-
-                localStorage.setItem(
-                    'shopping-cart',
-                    JSON.stringify(this.shoppingCart),
-                );
-            }
         },
 
         delete(product) {
--- src/views/pages/ShoppingCart.vue
+++ src/views/pages/ShoppingCart.vue
@@ -32,6 +32,10 @@ export default {
     data() {
         return {
             selectedProduct: null,
+
+            fees: {
+                taxPercentage: 25,
+            },
         };
     },
     computed: {
@@ -47,19 +51,22 @@ export default {
             return [
                 {
                     name: 'Ukupno',
-                    value: this.shoppingCartStore.totalWithoutFees,
+                    value: stringifyProductPrice(this.shoppingCartStore.total),
                 },
                 {
                     name: 'Rabat',
-                    value: this.shoppingCartStore.fees.delivery,
+                    value: 1,
+                    // value: this.shoppingCartStore.fees.delivery,
                 },
                 {
                     name: 'Porez',
-                    value: this.shoppingCartStore.fees.tax,
+                    value: 1,
+                    // value: this.shoppingCartStore.fees.tax,
                 },
                 {
                     name: 'Sveukupno',
-                    value: this.shoppingCartStore.totalWithFees,
+                    value: 1,
+                    // value: this.shoppingCartStore.totalWithFees,
                 },
             ];
         },
@@ -88,9 +95,7 @@ export default {
         },
 
         handlePrice(price) {
-            return stringifyProductPrice(
-                calcProductPrice(price, this.userStore.discount),
-            );
+            return stringifyProductPrice(calcProductPrice(price));
         },
 
         handleDeleteProduct(product) {
@@ -113,109 +118,119 @@ export default {
 </script>
 
 <template>
-    <div class="grid justify-content-between">
-        <div class="col">
-            <div class="card p-5 mb-0">
-                <DataTable
-                    v-if="
-                        shoppingCartStore.cart && shoppingCartStore.cart.length
-                    "
-                    :value="shoppingCartStore.cart"
-                >
-                    <Column field="image" header="Slika">
-                        <template #body="{ data }">
-                            <img
-                                :src="data.pictureUrl"
-                                class="table-image border-round cursor-pointer"
-                                @click="handleProductTableItemClick(data)"
-                            />
-                        </template>
-                    </Column>
+    <div
+        class="grid grid-nogutter justify-content-between bg-white border-round p-5 border-1 border-100"
+    >
+        <div class="col border-right-1 border-100 pr-5 mr-5">
+            <DataTable
+                id="shopping-cart"
+                :pt="{
+                    column: {
+                        headerCell: {
+                            class: 'bg-transparent',
+                        },
+                    },
+                    bodyRow: {
+                        style: 'border-color: red;'
+                    }
+                }"
+                v-if="shoppingCartStore.cart && shoppingCartStore.cart.length"
+                :value="shoppingCartStore.cart"
+            >
+                <Column field="image" header="Slika">
+                    <template #body="{ data }">
+                        <img
+                            :src="data.pictureUrl"
+                            class="table-image border-round cursor-pointer"
+                            @click="handleProductTableItemClick(data)"
+                        />
+                    </template>
+                </Column>
 
-                    <Column field="name" header="Proizvod">
-                        <template #body="{ data }">
-                            <span
-                                class="cursor-pointer"
-                                @click="handleProductTableItemClick(data)"
-                                >{{ data.name }}</span
-                            >
-                        </template>
-                    </Column>
+                <Column field="name" header="Proizvod">
+                    <template #body="{ data }">
+                        <span
+                            class="cursor-pointer"
+                            @click="handleProductTableItemClick(data)"
+                            >{{ data.name }}</span
+                        >
+                    </template>
+                </Column>
 
-                    <Column
-                        field="price"
-                        header="Cijena"
-                        style="min-width: 110px"
-                    >
-                        <template #body="{ data }">
-                            <span
-                                >{{
-                                    handlePrice(data.price * data.quantity)
-                                }}
-                                €</span
-                            >
-                        </template>
-                    </Column>
+                <Column field="price" header="Cijena" style="min-width: 110px">
+                    <template #body="{ data }">
+                        <span
+                            >{{
+                                handlePrice(data.price * data.quantity)
+                            }}
+                            €</span
+                        >
+                    </template>
+                </Column>
 
-                    <Column header="Količina">
-                        <template #body="{ data }">
-                            <InputNumber
-                                inputId="locale-german"
-                                locale="de-DE"
-                                v-model="data.quantity"
-                                inputStyle="width: 60px; text-align: center;"
-                                @update:modelValue="
-                                    handleNewProductQuantity(data)
-                                "
-                                @keyup.enter="handleNewProductQuantity(data)"
-                                min="1"
-                            />
-                        </template>
-                    </Column>
+                <Column header="Količina">
+                    <template #body="{ data }">
+                        <InputNumber
+                            inputId="locale-german"
+                            locale="de-DE"
+                            v-model="data.quantity"
+                            inputStyle="width: 60px; text-align: center;"
+                            @update:modelValue="handleNewProductQuantity(data)"
+                            @keyup.enter="handleNewProductQuantity(data)"
+                            min="1"
+                        />
+                    </template>
+                </Column>
 
-                    <Column header="Obriši">
-                        <template #body="{ data }">
-                            <Button
-                                class="button--no-shadow"
-                                icon="pi pi-trash"
-                                outlined
-                                severity="info"
-                                @click="handleDeleteProduct(data)"
-                            />
-                        </template>
-                    </Column>
-                </DataTable>
+                <Column header="Obriši">
+                    <template #body="{ data }">
+                        <Button
+                            class="button--no-shadow"
+                            icon="pi pi-trash"
+                            outlined
+                            severity="info"
+                            @click="handleDeleteProduct(data)"
+                        />
+                    </template>
+                </Column>
+            </DataTable>
 
-                <div v-else>Nema proizvoda u košarici...</div>
-            </div>
+            <div v-else>Nema proizvoda u košarici...</div>
         </div>
 
         <div
             v-if="shoppingCartStore.cart && shoppingCartStore.cart.length"
             class="col-12 lg:col-3"
         >
-            <div class="card p-5">
-                <div class="max-w-24rem mx-auto">
-                    <DataTable :value="order">
-                        <Column header="Ukupna narudžba">
-                            <template #body="{ data }">
-                                <div class="flex justify-content-between">
-                                    <span>{{ data.name }}</span>
-                                    <span class="text-right"
-                                        >{{ data.value }} €</span
-                                    >
-                                </div>
-                            </template>
-                        </Column>
-                    </DataTable>
-                    <Button
-                        class="button--no-shadow w-full mt-3"
-                        label="Završi narudžbu"
-                        outlined
-                        severity="info"
-                        @click="handleFinishOrderClick"
-                    />
-                </div>
+            <div class="max-w-24rem mx-auto">
+                <DataTable
+                    :value="order"
+                    :pt="{
+                        column: {
+                            headerCell: {
+                                class: 'bg-transparent',
+                            },
+                        },
+                    }"
+                >
+                    <Column header="Ukupna narudžba">
+                        <template #body="{ data }">
+                            <div class="flex justify-content-between">
+                                <span>{{ data.name }}</span>
+                                <span class="text-right"
+                                    >{{ data.value }} €</span
+                                >
+                            </div>
+                        </template>
+                    </Column>
+                </DataTable>
+                <Button
+                    class="button--no-shadow w-full mt-3"
+                    label="Završi narudžbu"
+                    outlined
+                    severity="info"
+                    @click="handleFinishOrderClick"
+                />
             </div>
         </div>
     </div>
